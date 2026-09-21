ec17cab58841e4a00d7253e1d90e4ceac48a9896
victor alvarez <victoralvgon2005@gmail.com>
2026-09-21 10:38:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myApp-blank',
+  webDir: 'www'
+};
+
+export default config;
