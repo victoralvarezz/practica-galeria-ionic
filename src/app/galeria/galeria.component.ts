2b7173137a218102f582f1e7bc79f3f3fbe0b6da
victor alvarez <victoralvgon2005@gmail.com>
2026-09-22 18:41:44 +0200

    Corregir compilacion para Vercel

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class GaleriaComponent implements OnInit {
 
-  // Aquí declaramos las variables
   public tituloSeccion: string = 'Mi Primera Galería';
   public rutaImagenLocal: string = 'assets/prueba1.jpg';
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+
   constructor() {}
+
 }
